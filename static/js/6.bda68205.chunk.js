46a2f807a419a6943927388f13581caa2ad468ca
Shuydo <shuydo12@gmail.com>
2021-08-26 21:09:24 +0300

    Updates

--- static/js/6.bda68205.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[6],{83:function(t,e,n){"use strict";n.r(e);n(0);var i=n(2),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};e.default=function(){return Object(i.jsx)("div",{style:c.container,children:Object(i.jsx)("h1",{style:c.title,children:"Hello"})})}}}]);
-//# sourceMappingURL=6.bda68205.chunk.js.map
